a25642d9f370ec149978abefa53bb17442a9da01
VladislavAkhmetov <119003958+VladislavAkhmetov@users.noreply.github.com>
2023-04-02 03:21:33 +0500

    правки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,23 @@
-let popup = document.querySelector('.popup'); // Тёмный фон
-let popupBody = document.querySelector('.popup__body'); // Окно
-let openPopupButton = document.querySelectorAll('.profile__button-edit'); // Кнопка редактирования
-let closePopupButton = document.querySelector('.popup__button-close'); // Кнопка закрытия
-let titleInput = document.querySelector('.popup__input_title'); // Присваиваем имя переменной введенной информации
-let subtitleInput = document.querySelector('.popup__input_subtitle'); // Присваиваем имя переменной введенной информации
-let profileTitle = document.querySelector('.profile__title'); // Присваиваем имя переменной полю profile__title
-let profileSubtitle = document.querySelector('.profile__subtitle'); // Присваиваем имя переменной полю profile__subtitle
-let saveButton = document.querySelector('.popup__button-submit'); // Имя переменной кнопки Сохранить
+let popup = document.querySelector('.popup');
+let openPopupButton = document.querySelector('.profile__button-edit');
+let closePopupButton = document.querySelector('.popup__button-close');
+let titleInput = document.querySelector('.popup__text-input_type_title');
+let subtitleInput = document.querySelector('.popup__text-input_type_subtitle');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let formSave = document.querySelector('.popup__form');
 
-openPopupButton.forEach((button) => { // Перебираем все кнопки
-    button.addEventListener('click', (event) => { // Для каждой вешаем обработчик событий на клик
-        event.preventDefault();
-        popup.classList.add('active');
-        popupBody.classList.add('active');
-        titleInput.value = profileTitle.textContent;
-        subtitleInput.value = profileSubtitle.textContent;
-    })
-});
-
-closePopupButton.addEventListener('click', () => { // При клике на крестик попап закрывается
-    popup.classList.remove('active');
-    popupBody.classList.remove('active');
-});
-
-document.addEventListener('click', (e) => { // При клике на фоне попап закрывается
-    if (e.target === popup) {
-        popup.classList.remove('active');
-        popupBody.classList.remove('active');
-    }
-});
+function popupOpen() {
+    popup.classList.add('active');
+    titleInput.value = profileTitle.textContent;
+    subtitleInput.value = profileSubtitle.textContent;
+};
+openPopupButton.addEventListener('click', popupOpen);
 
 function popupClose() {
     popup.classList.remove('active');
-    popupBody.classList.remove('active');
-}
+};
+closePopupButton.addEventListener('click', popupClose);
 
 function formSubmit(event) {
     event.preventDefault();
@@ -42,4 +26,4 @@ function formSubmit(event) {
     popupClose();
 };
 
-saveButton.addEventListener('click', formSubmit);
+formSave.addEventListener('submit', formSubmit);
